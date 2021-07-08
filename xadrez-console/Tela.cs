618418e1a69be3a65b77f03ad650561e00794daf
Marcelo Turler <marceloturler@hotmail.com>
2021-07-08 14:56:52 -0300

    Testes iniciais com movimentos possíveis das peças

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -25,31 +25,13 @@ namespace xadrez_console
                     if (j == i || ( j % 2 == 0 && i % 2 == 0 ) || (j % 2 != 0 && i % 2 != 0) ) {
                         Console.BackgroundColor = ConsoleColor.White;
                         Console.ForegroundColor = ConsoleColor.Black;
-                        if (tab.peca(i, j) == null)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            Console.Write("- ");
-                        }
-                        else
-                        {
-                            Tela.imprimirPeca(tab.peca(i, j));
-                            Console.Write(" ");
-                        }
+                        Tela.imprimirPeca(tab.peca(i, j));
                     }
                     else 
                     {
                         Console.BackgroundColor = ConsoleColor.Black;
                         Console.ForegroundColor = ConsoleColor.White;
-                        if (tab.peca(i, j) == null)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            Console.Write("- ");
-                        }
-                        else
-                        {
-                            Tela.imprimirPeca(tab.peca(i, j));
-                            Console.Write(" ");
-                        }
+                        Tela.imprimirPeca(tab.peca(i, j));
                     }
                 }
                 Console.BackgroundColor = ConsoleColor.Gray;
@@ -65,6 +47,75 @@ namespace xadrez_console
             Console.WriteLine("    A B C D E F G H");
         }
 
+        
+
+        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posPossiveis)
+        {
+            ConsoleColor fAlter = ConsoleColor.DarkGray;
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.WriteLine("                    ");
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(8 - i);
+                Console.Write(" ");
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.Write("  ");
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (checarCoresXadrez(j, i))
+                    {
+                        if (posPossiveis[i, j])
+                        {
+                            Console.BackgroundColor = fAlter;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.ForegroundColor = ConsoleColor.Black;
+                        }
+                        Tela.imprimirPeca(tab.peca(i, j));
+                    }
+                    else
+                    {
+                        if (posPossiveis[i, j])
+                        {
+                            Console.BackgroundColor = fAlter;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        Tela.imprimirPeca(tab.peca(i, j));
+                    }
+                }
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.Write("  ");
+                Console.WriteLine();
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.WriteLine("                    ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("    A B C D E F G H");
+
+            bool checarCoresXadrez(int j, int i)
+            {
+                if (j == i || (j % 2 == 0 && i % 2 == 0) || (j % 2 != 0 && i % 2 != 0))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
         public static PosicaoXadrez lerPosicaoXadrez()
         {
             string s = Console.ReadLine();
@@ -76,16 +127,29 @@ namespace xadrez_console
 
         public static void imprimirPeca(Peca peca)
         {
-            if (peca.cor == Cor.Branca)
+            if (peca == null )
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write(peca);
+                if (Console.BackgroundColor == ConsoleColor.DarkGray)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                Console.Write("- ");
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(peca);
+                if (peca.cor == Cor.Branca)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(peca);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(peca);
+                }
+                Console.Write(" ");
             }
+            
             Console.ForegroundColor = ConsoleColor.White;
         }
     }
